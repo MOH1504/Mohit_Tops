4596c73bc45d84f09893ccfdb35130357edae2ff
MOH1504 <113433954+MOH1504@users.noreply.github.com>
2022-12-20 15:15:15 +0530

    ok

---
+++ C Programming/Assignments/MODULE3.1.c/modue3.1q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ 
+void main()  
+{
+     printf("Name   : KISHAN THANKI\n"); 
+     printf("DOB    : SEPTEMBER 18, 2003\n"); 
+     printf("AGE    : 20\n"); 
+     printf("ADDRESS: ASHAPURA KRUPA RAMAN PARK GIDC ROAD PORBANDAR");
+}
